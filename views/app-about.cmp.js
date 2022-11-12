d6ebdcaad946b47725b8aef5a9c25d346b571254
Gil Wallach <gilwallach@gmail.com>
2022-11-12 22:56:00 +0200

    home & about added

--- views/app-about.cmp.js
+++ views/app-about.cmp.js
@@ -2,6 +2,10 @@ export default {
 	template: `
         <section class="about-page">
             <h1>About us</h1>
+            <div class="content-container">
+                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id rerum suscipit nostrum! Minus dolor provident porro quod, quis minima ab!</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia dolores aspernatur repudiandae numquam odio ipsa minus maxime accusamus nesciunt, facilis tenetur quia minima alias eius adipisci! Consequuntur quae inventore eligendi dolor. Similique nam maxime odio eum velit iste eaque error atque omnis placeat voluptates officiis enim exercitationem, culpa minus. Ab tempore quisquam, enim esse optio quaerat deleniti ducimus in nam rerum, asperiores velit numquam repudiandae molestias aliquid iure odio saepe ipsum obcaecati, praesentium nobis aliquam dignissimos. Maxime nisi est perferendis sed doloremque. Consequatur fuga nulla, placeat voluptatum accusantium animi nemo sapiente praesentium laborum aliquid reiciendis vitae recusandae excepturi perferendis iusto quaerat molestias quo magnam nesciunt dolore in? Impedit maxime, repellat odit architecto, blanditiis laborum rem voluptate nihil quo voluptatem sed unde, nisi aspernatur doloribus porro dolore. Iure ea vel repellat odio vitae asperiores aperiam provident placeat harum. Minima aliquam nam tenetur consequuntur voluptatem! Ut nulla quisquam quae fuga, rerum quibusdam consequuntur deserunt illum consectetur omnis voluptatum blanditiis veniam, dignissimos reiciendis doloribus dolores aliquid architecto at vero incidunt animi non dolorum, sit ipsa? Minima similique asperiores itaque distinctio, obcaecati in error dolorem aliquam facilis. Repudiandae odit ut at saepe impedit ad esse, inventore similique officiis tenetur pariatur dolorem possimus, adipisci quos.</p>
+            </div>
         </section>
     `
 }
--- views/app-home.cmp.js
+++ views/app-home.cmp.js
@@ -1,7 +1,11 @@
 export default {
 	template: `
-        <section class="home-page">
-            <h1>Home sweet home</h1>
+        <section class="home-page flex">
+            <div>
+                <h1>Welcome to <span>Appsus!</span></h1>
+                <h2>Developed by Bar & Gil</h2>
+            </div>
+            <img src="assets/img/icons/Appsus-logo.png" alt="" />
         </section>
     `,
 }
